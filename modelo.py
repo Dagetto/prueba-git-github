3147c622aca114c7327dcc2472ff05468a3273d6
Lucas Monzón <121673252+Dagetto@users.noreply.github.com>
2023-05-07 17:13:32 +0200

    ejercicio bases de datos
    creacion de base de datos R y NR

---
+++ modelo.py
@@ -0,0 +1,99 @@
+import sqlite3
+import re
+import pymongo
+from pymongo import MongoClient
+
+
+# ##############################################
+# MODELO
+# ##############################################
+class DataBase:
+    def __init__(
+        self,
+    ):
+        pass
+
+    def conexion_bd(self, d_val, e_val):
+        if e_val == "NR":
+            # self.conexion_mongo = self.client
+            self.client = pymongo.MongoClient("localhost", 27017)
+            self.db = self.client[d_val]
+            self.collection = self.db["test-collection"]
+            self.mi_diccionario = {"producto": "", "precio": "", "cantidad": ""}
+            self.registro = self.collection.insert_one(self.mi_diccionario)
+            print(self.registro)
+        elif e_val == "R":
+            self.con = sqlite3.connect(d_val)
+            cursor = self.con.cursor()
+            sql = """CREATE TABLE productos
+                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    producto varchar(20) NOT NULL,
+                    cantidad real,
+                    precio real)
+            """
+            cursor.execute(sql)
+            self.con.commit()
+        else:
+            pass
+
+
+class Abmc(
+    DataBase
+):  # No estoy seguro si ABMC debia ser una clase hija porque no esta especificado en el enunciado.
+    def alta(self, d_val, producto, cantidad, precio, tree):
+        cadena = producto
+        patron = "^[A-Za-záéíóú]*$"  # regex para el campo cadena
+        if re.match(patron, cadena):
+            print(producto, cantidad, precio)
+            self.con = sqlite3.connect(d_val)
+            cursor = self.con.cursor()
+            # con = self.conexion()
+            # cursor = con.cursor()
+            data = (producto, cantidad, precio)
+            sql = "INSERT INTO productos(producto, cantidad, precio) VALUES(?, ?, ?)"
+            cursor.execute(sql, data)
+            self.con.commit()
+            print("Estoy en alta todo ok")
+            self.actualizar_treeview(tree)
+        else:
+            print("error en campo producto")
+
+    def actualizar_treeview(self, mitreview):
+        records = mitreview.get_children()
+        for element in records:
+            mitreview.delete(element)
+
+        sql = "SELECT * FROM productos ORDER BY id ASC"
+        self.con = sqlite3.connect(d_val)
+        cursor = self.con.cursor()
+        datos = cursor.execute(sql)
+
+        resultado = datos.fetchall()
+        for fila in resultado:
+            print(fila)
+            mitreview.insert("", 0, text=fila[0], values=(fila[1], fila[2], fila[3]))
+
+
+"""
+    def consultar():
+        global compra
+        print(compra)
+
+def borrar(tree):
+    valor = tree.selection()
+    print(valor)   #('I005',)
+    item = tree.item(valor)
+    print(item)    #{'text': 5, 'image': '', 'values': ['daSDasd', '13.0', '2.0'], 'open': 0, 'tags': ''}
+    print(item['text'])
+    mi_id = item['text']
+
+    con=conexion()
+    cursor=con.cursor()
+    #mi_id = int(mi_id)
+    data = (mi_id,)
+    sql = "DELETE FROM productos WHERE id = ?;"
+    cursor.execute(sql, data)
+    con.commit()
+    tree.delete(valor)
+
+"""
---
+++ vista.py
@@ -0,0 +1,128 @@
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter import ttk
+import modelo
+
+
+# ##############################################
+# VISTA
+# ##############################################
+class Panel:
+    def __init__(self, window):
+        self.root = window
+        self.root.title("Tarea POO")
+        self.titulo = Label(
+            self.root,
+            text="Ingrese sus datos",
+            bg="DarkOrchid3",
+            fg="thistle1",
+            height=1,
+            width=60,
+        )
+        self.titulo.grid(row=0, column=0, columnspan=4, padx=1, pady=1, sticky=W + E)
+
+        try:
+            print("pasa codigo")
+            # self.objeto.conexion_mongo()
+            # self.objeto.crear_tablanr()
+            # self.objeto.conexion()
+            # self.objeto.crear_tabla()
+        except:
+            print("Error")
+        self.producto = Label(self.root, text="Producto")
+        self.producto.grid(row=1, column=0, sticky=W)
+        self.cantidad = Label(self.root, text="Cantidad")
+        self.cantidad.grid(row=2, column=0, sticky=W)
+        self.precio = Label(self.root, text="Precio")
+        self.precio.grid(row=3, column=0, sticky=W)
+        self.base_datos = Label(self.root, text="Nombre BD")
+        self.base_datos.grid(row=1, column=2, sticky=W)
+        self.tipo_base_datos = Label(self.root, text="Tipo de BD")
+        self.tipo_base_datos.grid(row=2, column=2, sticky=W)
+
+        # Defino variables para tomar valores de campos de entrada
+        self.a_val, self.b_val, self.c_val, self.d_val, self.e_val = (
+            StringVar(),
+            DoubleVar(),
+            DoubleVar(),
+            StringVar(),
+            StringVar(),
+        )
+        w_ancho = 20
+
+        self.entrada1 = Entry(self.root, textvariable=self.a_val, width=w_ancho)
+        self.entrada1.grid(row=1, column=1)
+        self.entrada2 = Entry(self.root, textvariable=self.b_val, width=w_ancho)
+        self.entrada2.grid(row=2, column=1)
+        self.entrada3 = Entry(self.root, textvariable=self.c_val, width=w_ancho)
+        self.entrada3.grid(row=3, column=1)
+        self.entrada4 = Entry(self.root, textvariable=self.d_val, width=w_ancho)
+        self.entrada4.grid(row=1, column=3)
+        self.entrada5 = Entry(self.root, textvariable=self.e_val, width=w_ancho)
+        self.entrada5.grid(row=2, column=3)
+
+        # -------------------------------------------------
+        self.objeto1 = modelo.Abmc()
+        self.objeto = modelo.DataBase()
+        # self.objeto = modelo.DataBase()
+
+        # --------------------------------------------------
+        # TREEVIEW
+        # --------------------------------------------------
+
+        self.tree = ttk.Treeview(self.root)
+        self.tree["columns"] = ("col1", "col2", "col3")
+        self.tree.column("#0", width=90, minwidth=50, anchor=W)
+        self.tree.column("col1", width=200, minwidth=80)
+        self.tree.column("col2", width=200, minwidth=80)
+        self.tree.column("col3", width=200, minwidth=80)
+        self.tree.heading("#0", text="ID")
+        self.tree.heading("col1", text="Producto")
+        self.tree.heading("col2", text="cantidad")
+        self.tree.heading("col3", text="precio")
+        self.tree.grid(row=10, column=0, columnspan=4)
+
+        self.boton_alta = Button(
+            self.root,
+            text="Alta",
+            command=lambda: self.objeto1.alta(
+                self.a_val.get(),
+                self.b_val.get(),
+                self.c_val.get(),
+                self.tree,
+            ),
+        )
+        self.boton_alta.grid(row=6, column=1)
+
+        self.boton_bd = Button(
+            self.root,
+            text="conectar/crear bd",
+            command=lambda: self.objeto.conexion_bd(
+                self.d_val.get(),
+                self.e_val.get(),
+            ),
+        )
+        self.boton_bd.grid(row=6, column=3)
+
+
+"""
+
+
+
+
+boton_consulta=Button(root, text="Consultar", command=lambda:modelo.consultar())
+boton_consulta.grid(row=7, column=1)
+
+boton_borrar=Button(root, text="Borrar", command=lambda:modelo.borrar(tree))
+boton_borrar.grid(row=8, column=1)
+
+ 
+"""
+
+# #####################################
+# Para mi futuro controlador
+# #####################################
+if __name__ == "__main__":
+    root = Tk()
+    mi_app = Panel(root)
+    root.mainloop()
